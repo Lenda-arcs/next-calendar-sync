97005eca18b30fbebed089528219976a39f4e42d
Lennart Diedrichsen <lennart.diedrichsen@gmail.com>
2025-07-30 11:51:26 +0200

    refactor: remove outdated TODO comment in TeacherStudioRequest component
    - Eliminated the TODO comment regarding migration to the new query system, streamlining the code and improving clarity.

--- src/components/dashboard/components/TeacherStudioRequest.tsx
+++ src/components/dashboard/components/TeacherStudioRequest.tsx
@@ -17,7 +17,6 @@ export function TeacherStudioRequest({ userId }: TeacherStudioRequestProps) {
   const { t } = useTranslationNamespace('dashboard.studioRequest')
   const [showRequestDialog, setShowRequestDialog] = useState(false)
 
-    //TODO: CURSOR Migrate to new query system
   // Fetch teacher's studio relationships
   const { data: teacherRelationships, isLoading } = useTeacherStudioRelationships({
     teacherId: userId,
